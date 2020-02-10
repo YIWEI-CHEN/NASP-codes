a0b2f5215aa8c65dfdf10f3119afa8aa66158c0c
Yi-Wei Chen <feberium@gmail.com>
2020-02-10 11:30:03 -0600

    Use root logger and tensorboard_logger

--- codes/scripts/eval.sh
+++ codes/scripts/eval.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GPU=0
-TRAIN_BATCH_SIZE=128
+TRAIN_BATCH_SIZE=136
 VALID_BATCH_SIZE=64
 SEED=0
 EXP_PATH="exp/single/train_bz${TRAIN_BATCH_SIZE}_valid_bz${VALID_BATCH_SIZE}_gpu${GPU}"
@@ -9,5 +9,5 @@ EPOCH=600
 ARCH="SINGLE_NASP"
 
 python train.py --data /home/yiwei/cifar10 --gpu ${GPU} --save ${EXP_PATH} \
-    --seed ${SEED} --epochs ${EPOCH} --arch ${ARCH} \
+    --seed ${SEED} --epochs ${EPOCH} --arch ${ARCH} --workers 0 --cutout --auxiliary \
     --valid_batch_size ${VALID_BATCH_SIZE} --train_batch_size ${TRAIN_BATCH_SIZE}
--- codes/train.py
+++ codes/train.py
@@ -4,16 +4,15 @@ import time
 import glob
 import numpy as np
 import torch
-import utils
 import logging
 import argparse
 import torch.nn as nn
-import genotypes
 import torch.utils
 import torchvision.datasets as dset
-import torch.backends.cudnn as cudnn
+from tensorboard_logger import configure, log_value
 
-from torch.autograd import Variable
+import utils
+import genotypes
 from model import NetworkCIFAR as Network
 
 
@@ -40,6 +39,10 @@ parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='NASP', help='which architecture to use')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 parser.add_argument('--exec_script', type=str, default='scripts/eval.sh', help='script to run exp')
+parser.add_argument('--name', type=str, default="runs", help='name for log')
+parser.add_argument('--train_portion', type=float, default=0.9, help='portion of training data')
+parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
+                    help='number of data loading workers (default: 1)')
 args = parser.parse_args()
 
 args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
@@ -48,27 +51,33 @@ utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'), exec_script=a
 # Logging configuration
 utils.setup_logger(args)
 
+# tensorboard_logger configuration
+configure('{}/{}'.format(args.save, args.name))
+
+
 CIFAR_CLASSES = 10
 
 os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
 
 
 def main():
+  root = logging.getLogger()
+
   if not torch.cuda.is_available():
-    logging.info('no gpu device available')
+    root.info('no gpu device available')
     sys.exit(1)
 
   # Fix seed
   utils.fix_seed(args.seed)
 
-  logging.info('gpu device = %d' % args.gpu)
-  logging.info("args = %s", args)
+  root.info('gpu device = %d' % args.gpu)
+  root.info("args = %s", args)
 
   genotype = eval("genotypes.%s" % args.arch)
   model = Network(args.init_channels, CIFAR_CLASSES, args.layers, args.auxiliary, genotype)
   model = model.cuda()
 
-  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+  root.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
   criterion = nn.CrossEntropyLoss()
   criterion = criterion.cuda()
@@ -79,33 +88,47 @@ def main():
       weight_decay=args.weight_decay
       )
 
-  train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
-  valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
-
-  train_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.train_batch_size, shuffle=True, pin_memory=True, num_workers=0)
-
-  valid_queue = torch.utils.data.DataLoader(
-      valid_data, batch_size=args.valid_batch_size, shuffle=False, pin_memory=True, num_workers=0)
+  # Data loading code
+  train_queue, train_sampler, valid_queue = get_train_validation_loader(args)
+  test_queue = get_test_loader(args)
 
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
 
+  best_acc = 0
   for epoch in range(args.epochs):
     scheduler.step()
-    logging.info('epoch %d lr %e', epoch, scheduler.get_lr()[0])
+    lr = scheduler.get_lr()[0]
+    log_value("lr", lr, epoch)
+    root.info('epoch %d lr %e', epoch, lr)
     model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
 
+    # train
     train_acc, train_obj = train(train_queue, model, criterion, optimizer)
-    logging.info('train_acc %f', train_acc)
+    root.info('train_acc %f, train_loss %f', train_acc, train_obj)
+    log_value('train_acc', train_acc, epoch)
 
+    # valid
     valid_acc, valid_obj = infer(valid_queue, model, criterion)
-    logging.info('valid_acc %f', valid_acc)
+    root.info('valid_acc %f, valid_obj %f', valid_acc, valid_obj)
+    log_value('valid_acc', valid_acc, epoch)
+
+    # test
+    test_acc, test_obj = infer(test_queue, model, criterion)
+    root.info('test_acc %f, test_obj %f', test_acc, test_obj)
+    log_value('test_acc', test_acc, epoch)
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
 
+    is_best = valid_acc > best_acc
+    best_acc = max(valid_acc, best_acc)
+    if is_best:
+      root.info('best test_acc: {} at Epoch {} when valid_acc is {}'.format(
+        test_acc, epoch, best_acc
+      ))
+
 
 def train(train_queue, model, criterion, optimizer):
+  root = logging.getLogger()
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
@@ -132,12 +155,13 @@ def train(train_queue, model, criterion, optimizer):
     top5.update(prec5.item(), n)
 
     if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+      root.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
 
   return top1.avg, objs.avg
 
 
 def infer(valid_queue, model, criterion):
+  root = logging.getLogger()
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
@@ -157,9 +181,42 @@ def infer(valid_queue, model, criterion):
     top5.update(prec5.item(), n)
 
     if step % args.report_freq == 0:
-      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+      root.info('%s %03d %e %f %f', valid_queue.name, step, objs.avg, top1.avg, top5.avg)
 
   return top1.avg, objs.avg
+
+
+def get_train_validation_loader(args):
+  train_transform, valid_transform = utils._data_transforms_cifar10(args)
+  train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
+
+  num_train = len(train_data)
+  indices = list(range(num_train))
+  split = int(np.floor(args.train_portion * num_train))
+
+  # train[0:split] as training data
+  train_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices[:split])
+  train_queue = torch.utils.data.DataLoader(
+    train_data, batch_size=args.train_batch_size,
+    num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+  # train[split:] as validation data
+  valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(indices[split:])
+  valid_queue = torch.utils.data.DataLoader(
+    train_data, batch_size=args.valid_batch_size,
+    num_workers=args.workers, pin_memory=True, sampler=valid_sampler)
+  valid_queue.name = 'valid'
+
+  return train_queue, train_sampler, valid_queue
+
+
+def get_test_loader(args):
+  _, test_transform = utils._data_transforms_cifar10(args)
+  test_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=test_transform)
+  test_queue = torch.utils.data.DataLoader(
+    test_data, batch_size=args.valid_batch_size, shuffle=False, pin_memory=True, num_workers=args.workers)
+  test_queue.name = 'test'
+  return test_queue
 
 
 if __name__ == '__main__':
