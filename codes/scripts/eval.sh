caedac3a6a647292b6a6d41e0be204bde254657c
Yi-Wei Chen <feberium@gmail.com>
2020-07-05 15:01:52 -0500

    Add SINGLE_NASP_0701 and SINGLE_BIN_ARCH_0704 for evaluation

--- codes/scripts/eval.sh
+++ codes/scripts/eval.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-GPU=1
-TRAIN_BATCH_SIZE=136
-VALID_BATCH_SIZE=64
+GPU=2
+TRAIN_BATCH_SIZE=96
+VALID_BATCH_SIZE=96
 SEED=0
 EPOCH=600
-ARCH="SINGLE_NASP"
-PORTION=1.0
+ARCH="SINGLE_NASP_0701"
+PORTION=0.9
 EXP_PATH="exp/single/${ARCH}/${PORTION}train_bz${TRAIN_BATCH_SIZE}_valid_bz${VALID_BATCH_SIZE}_gpu${GPU}"
 
 python train.py --data /home/yiwei/cifar10 --gpu ${GPU} --save ${EXP_PATH} \
